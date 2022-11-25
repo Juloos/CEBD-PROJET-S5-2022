c570870f8132cb89a32ce3dc8f9ee6e2751f5c8b
Juloos <juloos.sfr@gmail.com>
2022-11-25 08:45:45 +0100

    1.2 Done

--- actions/v0_action_fct_comp_2_partie_1.py
+++ actions/v0_action_fct_comp_2_partie_1.py
@@ -13,21 +13,22 @@ class AppFctComp2Partie1(QDialog):
         super(QDialog, self).__init__()
         self.ui = uic.loadUi("gui/fct_comp_2.ui", self)
         self.data = data
+        # on récupère les catégories
+        for categorie in self.data.cursor().execute("SELECT DISTINCT categorieEp FROM V0_LesEpreuves"):
+            self.ui.comboBox_fct_comp_2.addItem(categorie[0])
 
     # Fonction de mise à jour de l'affichage
     def refreshResult(self):
-        # TODO 1.2 : fonction à modifier pour remplacer la zone de saisie par une liste de valeurs issues de la BD une
-        #  fois le fichier ui correspondant mis à jour
         display.refreshLabel(self.ui.label_fct_comp_2, "")
-        if not self.ui.lineEdit_fct_comp_2.text().strip():
+        if not self.ui.comboBox_fct_comp_2.currentText().strip():
             self.ui.table_fct_comp_2.setRowCount(0)
             display.refreshLabel(self.ui.label_fct_comp_2, "Veuillez indiquer un nom de catégorie")
         else:
             try:
                 cursor = self.data.cursor()
                 result = cursor.execute(
-                    "SELECT numEp, nomEp, formeEp, nomDi, categorieEp, nbSportifsEp, strftime('%Y-%m-%d',dateEp,'unixepoch') FROM LesEpreuves WHERE categorieEp = ?",
-                    [self.ui.lineEdit_fct_comp_2.text().strip()])
+                    "SELECT numEp, nomEp, formeEp, nomDi, categorieEp, nbSportifsEp, strftime('%Y-%m-%d',dateEp,'unixepoch') FROM V0_LesEpreuves WHERE categorieEp = ?",
+                    [self.ui.comboBox_fct_comp_2.currentText().strip()])
             except Exception as e:
                 self.ui.table_fct_comp_3.setRowCount(0)
                 display.refreshLabel(self.ui.label_fct_comp_2, "Impossible d'afficher les résultats : " + repr(e))
