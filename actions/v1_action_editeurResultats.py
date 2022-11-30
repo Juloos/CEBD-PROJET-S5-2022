effee44a4cfbd1a0fb940a425b333d8bff4dc4e1
Juloos <juloos.sfr@gmail.com>
2022-12-01 00:12:49 +0100

    Commentaires ajoutés

--- actions/v1_action_editeurResultats.py
+++ actions/v1_action_editeurResultats.py
@@ -15,6 +15,9 @@ class AppEditeurResultatsV1(QDialog):
         self.data = data
 
         # attribuer les valeurs aux comboBox
+        # les contraintes d'intégrité suivantes ne sont pas prises en compte dans la conception de l'interface :
+        #   - les participants d'une épreuve doivent être du genre que la catégorie de l'épreuve comprend
+        #   - le type des participants (individuel ou équipe) doit être le même que celui de l'épreuve
         self.refreshAll()
 
     def tryquery(self, query):
