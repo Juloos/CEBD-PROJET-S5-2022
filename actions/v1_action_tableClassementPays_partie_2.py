129a0b9d116a5126acad29cdace7228a3305768f
Ascanaki <lancelin.foussadier@gmail.com>
2022-11-30 19:49:15 +0100

    REQUETE SQL POUR LE CLASSEMENT PAR PAYS FINI !!!!!

--- actions/v1_action_tableClassementPays_partie_2.py
+++ actions/v1_action_tableClassementPays_partie_2.py
@@ -22,9 +22,31 @@ class AppTableClassementPaysV1(QDialog):
         display.refreshLabel(self.ui.label_tableClassementPays, "")
         try:
             cursor = self.data.cursor()
-            result = cursor.execute("SELECT numEq, 1, 1, 1, 3 FROM LesEquipes")
+            #requete sql pour obtenir tout les pays
+            result = cursor.execute("WITH Pays AS (SELECT DISTINCT pays FROM LesSportifs),"
+                                     "nbOS AS (SELECT pays, COUNT(medailleOr) AS NO FROM LesEpreuves "
+                                     "JOIN LesSportifs ON (numSp = medailleOr) GROUP BY pays),"
+                                     "nbOE AS (SELECT paysEQ, COUNT(medailleOr) AS NOE FROM LesEpreuves "
+                                     "JOIN LesEquipes ON (medailleOr = numEq) GROUP BY paysEQ),"
+                                     "nbAS AS (SELECT pays, COUNT(medailleArgent) AS NA FROM LesEpreuves "
+                                     "JOIN LesSportifs ON (numSp = medailleArgent) GROUP BY pays),"
+                                     "nbAE AS (SELECT paysEQ, COUNT(medailleArgent) AS NAE FROM LesEpreuves "
+                                     "JOIN LesEquipes ON (medailleArgent = numEq) GROUP BY paysEQ),"
+                                     "nbBS AS (SELECT pays, COUNT(medailleBronze) AS NB FROM LesEpreuves "
+                                     "JOIN LesSportifs ON (numSp = medailleBronze) GROUP BY pays),"
+                                     "nbBE AS (SELECT paysEQ, COUNT(medailleBronze) AS NBE FROM LesEpreuves "
+                                     "JOIN LesEquipes ON (medailleBronze = numEq) GROUP BY paysEQ)"
+                                     "SELECT Pays.pays, ifnull(NO,0) + ifnull(NOE,0) AS nbO, "
+                                     "ifnull(NA,0) + ifnull(NAE,0) AS nbA, ifnull(NB,0) + ifnull(NBE,0) AS nbB,"
+                                     "ifnull(NO,0) + ifnull(NOE,0) + ifnull(NA,0) + ifnull(NAE,0) "
+                                     "+ ifnull(NB,0) + ifnull(NBE,0) AS nbM "
+                                     "FROM Pays "
+                                     "LEFT JOIN nbOS USING (pays) LEFT JOIN (nbOE) ON(pays =nbOE.paysEQ)"
+                                     "LEFT JOIN nbAS USING (pays) LEFT JOIN (nbAE) ON(pays =nbAE.paysEQ)"
+                                     "LEFT JOIN nbBS USING (pays) LEFT JOIN (nbBE) ON(pays =nbBE.paysEQ)"
+                                     "ORDER BY nbM DESC,nbO DESC, nbA DESC, nbB DESC;")
+
         except Exception as e:
-            self.ui.label_tableClassementPays.setRowCount(0)
             display.refreshLabel(self.ui.label_tableClassementPays, "Impossible d'afficher les résultats : " + repr(e))
         else:
             display.refreshGenericData(self.ui.tableClassementPaysV1, result)
